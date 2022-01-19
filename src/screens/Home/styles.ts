55d752b1b28749f34ace145ed504391ab295019b
Alexandre Junior <alexandre.junior.ext@neon.com.br>
2022-01-19 14:59:46 -0300

    delete past

--- src/screens/Home/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #F2F2F2; 
-`;
